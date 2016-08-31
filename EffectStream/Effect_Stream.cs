06dc6bc415d929be7c3944f0e19519b107523921
Chausiku32 <nixmunge@gmail.com>
2016-08-31 21:43:05 +0300

    EffectStream
    Works

---
+++ EffectStream/Effect_Stream.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using NAudio.Wave;
+using System.Collections.Generic;
+
+namespace EffectStream
+{
+    class Effect_Stream : WaveStream
+    {
+        public WaveStream SourceStream
+        {
+            get;
+            set;
+        }
+
+        public List<IEffect> Effect { get; private set; }
+
+        public Effect_Stream(WaveStream stream)
+        {
+            this.SourceStream = stream;
+            this.Effect = new List<IEffect>();
+        }
+
+        public override long Length
+        {
+            get { return SourceStream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return SourceStream.Position; }
+            set { SourceStream.Position = value; }
+        }
+
+        public override WaveFormat WaveFormat
+        {
+            get { return SourceStream.WaveFormat; }
+        }
+
+        private int channel = 0;
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            Console.WriteLine("DirectSoundOut requested {0} bytes", count);
+
+            int read = SourceStream.Read(buffer,offset,count);
+
+            for (int i = 0; i < read / 4; i++)
+            {
+                float sample = BitConverter.ToSingle(buffer, i*4);
+
+                if (Effect.Count == WaveFormat.Channels) 
+                {
+                    sample = Effect[channel].ApplyEffect(sample);
+                    channel = (channel + 1) % WaveFormat.Channels;
+                }
+
+                byte[] bytes = BitConverter.GetBytes(sample);
+                buffer[i * 4 + 0] = bytes[0];
+                buffer[i * 4 + 1] = bytes[1];
+                buffer[i * 4 + 2] = bytes[2];
+                buffer[i * 4 + 3] = bytes[3];
+            }
+                return read;
+        }
+
+    }
+}
---
+++ EffectStream/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+using NAudio.Wave;
+
+namespace EffectStream
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DirectSoundOut output = null;
+
+        private BlockAlignReductionStream stream = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "WAV File (*.wav)|*.wav;";
+            if(open.ShowDialog() != DialogResult.OK) return;
+
+            WaveChannel32 wave = new WaveChannel32(new WaveFileReader(open.FileName));
+            Effect_Stream effect = new Effect_Stream(wave);
+            stream = new BlockAlignReductionStream(effect);
+
+            for (int i = 0; i < wave.WaveFormat.Channels; i++) effect.Effect.Add(new Echo());
+
+                output = new DirectSoundOut(200);
+            output.Init(stream);
+            output.Play();
+
+        }
+
+
+    }
+}
